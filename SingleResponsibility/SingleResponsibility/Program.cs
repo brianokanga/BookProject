ebc4b76a2f741f31c793cbc1b39e8b3081db04a3
Brian Okanga <briannokanga@gmail.com>
2021-09-28 15:15:02 +0300

    Reviewed Single responsibility

--- SingleResponsibility/SingleResponsibility/Program.cs
+++ SingleResponsibility/SingleResponsibility/Program.cs
@@ -5,15 +5,16 @@ namespace SingleResponsibility
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var journal = new Journal();
             journal.AddEntry("I was stressed today");
             journal.AddEntry("I was found today");
+
             Console.WriteLine(journal);
 
             var p = new Persistence();
-            var filename = @"C:\Projects\journal.text";
+            var filename = @"C:\Projects\journal.txt";
 
             p.SaveToFile(journal,filename,true);
             Process.Start(filename);
